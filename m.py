ad950d1bc5db719a091c6f1b185d7f697ba35f92
monendher22 <52447170+monendher22@users.noreply.github.com>
2019-07-03 09:55:27 +0530

    Update m.py

--- m.py
+++ m.py
@@ -1,1 +1,9 @@
-jhfdjuwhf
+vowel = ['a','e','i','o','u']
+consonant = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+ch = input('')
+if ch in vowel:
+  print('Vowel')
+elif ch in consonant:
+  print('Consonant')
+else:
+  print('Invalid')
