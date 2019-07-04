8b39f7e47a70414f97a398e086037989fb1cc611
monendher22 <52447170+monendher22@users.noreply.github.com>
2019-07-04 15:20:40 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,11 @@
+s = input()
+d = input()
+if (s == 'Sunday') or (s == 'Saturday'):
+  print('yes')
+else:
+  print('no')
+if (d == 'Sunday') or (s == 'Saturday'):
+  print('yes')
+else:
+  print('no')
+
