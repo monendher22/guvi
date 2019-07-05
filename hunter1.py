10a4f460805acd110dabf36d105bc11a6b669209
monendher22 <52447170+monendher22@users.noreply.github.com>
2019-07-05 09:48:45 +0530

    Update hunter1.py

--- hunter1.py
+++ hunter1.py
@@ -1,6 +1,6 @@
 def reverseWordSentence(Sentence):
   words = Sentence.split('')
   newwords = [word[::-1] for word in words]
-  newSentence = ''.jion(newwords)
+  newSentence = ''.join(newwords)
   return newSentence
 print(reverseWordSentence(input()))
